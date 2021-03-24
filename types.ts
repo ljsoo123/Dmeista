9bfeb0656886d312629693a0161ddf17bfb7506a
ljsoo123 <ljsoo123456@naver.com>
2021-03-24 12:04:57 +0900

    [CREATE] type export

---
+++ types.ts
@@ -0,0 +1,7 @@
+export type titleType = number | string;
+export type nicknameType = string;
+export type hashTagType = string[];
+export type timeType = string;
+export type writeType = string;
+export type viewsType = number;
+export type likeType = number;
