45840258225edb37cda518d113fb10b08e7da8d2
ljsoo123 <ljsoo123456@naver.com>
2021-03-29 19:27:25 +0900

    [MODIFY] header 고정

--- src/styles.ts
+++ src/styles.ts
@@ -3,9 +3,10 @@ import styled, { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 
 body{
-    margin: 0 auto;
+    margin: 0;
     @import url(http://fonts.googleapis.com/earlyaccess/notosanskr.css);
-font-family: "Noto Sans KR", sans-serif;
-background-color: #fbfbfb
+    font-family: "Noto Sans KR", sans-serif;
+    background-color: #fbfbfb;
+    padding-top:50px;
 }
-        `;
+`;
