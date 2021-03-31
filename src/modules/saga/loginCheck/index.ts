0dbee66c37b828a7134ce4f2deec3f2b9093f4e4
ljsoo123 <ljsoo123456@naver.com>
2021-03-31 11:50:24 +0900

    [MODIFY] add signup generator

--- src/modules/saga/loginCheck/index.ts
+++ src/modules/saga/loginCheck/index.ts
@@ -7,19 +7,27 @@ import {
   MODALSTATE,
   MODALSTATE_SAGA,
   modalState,
+  SIGNUPSTATE,
+  SIGNUPSTATE_SAGA,
+  signUpState,
 } from "../../action/loginCheck";
 
 function* loginStateSagaFunc() {
   yield put(loginState());
 }
 
+function* modalStateSagaFunc() {
+  yield put(modalState());
+}
+
+function* signUpStateSagaFunc() {
+  yield put(signUpState());
+}
+
 function* loginSaga() {
   yield takeEvery(LOGINSTATE_SAGA, loginStateSagaFunc);
   yield takeEvery(MODALSTATE_SAGA, modalStateSagaFunc);
-}
-
-function* modalStateSagaFunc() {
-  yield put(modalState());
+  yield takeEvery(SIGNUPSTATE_SAGA, signUpStateSagaFunc);
 }
 
 export default loginSaga;
