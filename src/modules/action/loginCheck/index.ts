aca37ffe879575ef2c82c7bf6510b74e2b62b139
ljsoo123 <ljsoo123456@naver.com>
2021-03-31 11:51:06 +0900

    [MODIFY] add signup actions

--- src/modules/action/loginCheck/index.ts
+++ src/modules/action/loginCheck/index.ts
@@ -4,6 +4,8 @@ export const USERSTATE = "mypage/USERSTATE";
 export const USERSTATE_SAGA = "mypage/USERSTATE_SAGA";
 export const MODALSTATE = "LOGIN/MODAL";
 export const MODALSTATE_SAGA = "LOGIN/MODAL_SAGA";
+export const SIGNUPSTATE = "SIGNUP/SIGNUPSTATE";
+export const SIGNUPSTATE_SAGA = "SIGNUP/SIGNUPSTATE_SAGA";
 
 export const loginState = () => ({ type: LOGINSTATE });
 export const loginStateSaga = () => ({ type: LOGINSTATE_SAGA });
@@ -11,9 +13,11 @@ export const userState = () => ({ type: USERSTATE });
 export const userStateSaga = () => ({ type: USERSTATE_SAGA });
 export const modalState = () => ({ type: MODALSTATE });
 export const modalStateSaga = () => ({ type: MODALSTATE_SAGA });
+export const signUpState = () => ({ type: SIGNUPSTATE });
+export const signUpStateSaga = () => ({ type: SIGNUPSTATE_SAGA });
 
 type LoginCheckAction = ReturnType<
-  typeof loginState | typeof userState | typeof modalState
+  typeof loginState | typeof userState | typeof modalState | typeof signUpState
 >;
 
 export default LoginCheckAction;
