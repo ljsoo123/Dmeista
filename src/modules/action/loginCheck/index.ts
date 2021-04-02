a14e5a4da8a9c767289b5cc595c4f6d1db39e328
ljsoo123 <ljsoo123456@naver.com>
2021-04-02 16:14:29 +0900

    [MODIFY] add friend action

--- src/modules/action/loginCheck/index.ts
+++ src/modules/action/loginCheck/index.ts
@@ -1,23 +1,36 @@
 export const LOGINSTATE = "header/LOGIN";
 export const LOGINSTATE_SAGA = "header/LOGINSTATE_SAGA";
-export const USERSTATE = "mypage/USERSTATE";
-export const USERSTATE_SAGA = "mypage/USERSTATE_SAGA";
 export const MODALSTATE = "LOGIN/MODAL";
 export const MODALSTATE_SAGA = "LOGIN/MODAL_SAGA";
 export const SIGNUPSTATE = "SIGNUP/SIGNUPSTATE";
 export const SIGNUPSTATE_SAGA = "SIGNUP/SIGNUPSTATE_SAGA";
+export const FRIENDSTATE = "FRIENDREQUEST/FRIENDSTATE";
+export const FRIENDSTATE_SAGA = "FRIENDREQUEST/FRIENDSTATE_SAGA";
+export const FRIENDCHECK = "REQUESTLIST/FRIENDREQUEST";
+export const FRIENDCHECK_SAGA = "REQUESTLIST/FRIENDREQUEST_SAGA";
+export const FRIENDDEL = "REQUESTLIST/FRIENDDEL";
+export const FRIENDDEL_SAGA = "REQUESTLIST/FRIENDDEL_SAGA";
 
 export const loginState = () => ({ type: LOGINSTATE });
 export const loginStateSaga = () => ({ type: LOGINSTATE_SAGA });
-export const userState = () => ({ type: USERSTATE });
-export const userStateSaga = () => ({ type: USERSTATE_SAGA });
 export const modalState = () => ({ type: MODALSTATE });
 export const modalStateSaga = () => ({ type: MODALSTATE_SAGA });
 export const signUpState = () => ({ type: SIGNUPSTATE });
 export const signUpStateSaga = () => ({ type: SIGNUPSTATE_SAGA });
+export const friendState = () => ({ type: FRIENDSTATE });
+export const friendStateSaga = () => ({ type: FRIENDSTATE_SAGA });
+export const friendRequest = () => ({ type: FRIENDCHECK });
+export const friendRequestSaga = () => ({ type: FRIENDCHECK_SAGA });
+export const friendDel = () => ({ type: FRIENDDEL });
+export const friendDelSaga = () => ({ type: FRIENDDEL_SAGA });
 
 type LoginCheckAction = ReturnType<
-  typeof loginState | typeof userState | typeof modalState | typeof signUpState
+  | typeof loginState
+  | typeof modalState
+  | typeof signUpState
+  | typeof friendState
+  | typeof friendRequest
+  | typeof friendDel
 >;
 
 export default LoginCheckAction;
