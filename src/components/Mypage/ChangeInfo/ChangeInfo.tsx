8d28863be3300d9ae8090ede8132bbb73c70134a
ljsoo123 <ljsoo123456@naver.com>
2021-04-05 10:10:03 +0900

    [CREATE] changeInfo

---
+++ src/components/Mypage/ChangeInfo/ChangeInfo.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import * as S from "../../../styles/ChangeInfoStyle";
+import { useSelector, useDispatch } from "react-redux";
+import { Store } from "../../../modules/reducer";
+import {
+  changeInfoSaga,
+  changePasswordSaga,
+} from "../../../modules/action/loginCheck";
+import cancelLogin from "../../../../image/cancelLogin.svg";
+
+const ChangeInfo = (props: { passwordCheck: boolean }) => {
+  const { passwordCheck } = props;
+  const dispatch = useDispatch();
+  const changeInfo = useSelector((store: Store) => store.loginCheck.changeInfo);
+  const onXClick = () => {
+    dispatch(changeInfoSaga());
+  };
+  const onPasswordClick = () => {
+    dispatch(changeInfoSaga());
+    dispatch(changePasswordSaga());
+  };
+  const onEmailClick = () => {
+    dispatch(changeInfoSaga());
+  };
+  return (
+    <>
+      {changeInfo && (
+        <S.Main>
+          <S.MainDiv>
+            <S.Content>
+              <div>
+                <S.Image>
+                  <img src={cancelLogin} onClick={onXClick}></img>
+                </S.Image>
+              </div>
+              <S.Header>회원 정보 수정</S.Header>
+              <S.BorderDiv />
+              <S.ChangeButton onClick={onPasswordClick}>
+                <div>패스워드 변경</div>
+                <div>인증 후 패스워드를 재설정 합니다.</div>
+              </S.ChangeButton>
+              <S.ChangeButton onClick={onEmailClick}>
+                <div>이메일 변경</div>
+                <div>인증 후 이메일을 재설정 합니다.</div>
+              </S.ChangeButton>
+            </S.Content>
+          </S.MainDiv>
+        </S.Main>
+      )}
+    </>
+  );
+};
+
+export default ChangeInfo;
