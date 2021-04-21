efb3706b9326d3ced2fbb1201be5229e1a04baf6
ljsoo123 <ljsoo123456@naver.com>
2021-04-21 22:48:07 +0900

    [CREATE] postContent style

---
+++ src/styles/PostContentStyle.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+export const Main = styled.div`
+  width: 100vw;
+  padding-top: 100px;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 1;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background: #b9b9b978;
+  backdrop-filter: blur(2px);
+`;
+
+export const MainDiv = styled.div`
+  width: 900px;
+  transform: translateY(-25%);
+  @media (max-width: 1200px) {
+    width: 40%;
+  }
+  @media (max-width: 600px) {
+    width: 90%;
+  }
+  height: 450px;
+  background-color: white;
+  border: 0.5px solid #707070;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  opacity: 1;
+`;
+
+export const ImageDiv = styled.div`
+  background: black;
+  width: 50%;
+  height: 100%;
+`;
+
+export const ContentDiv = styled.div`
+  background: white;
+  width: 50%;
+  height: 100%;
+`;
