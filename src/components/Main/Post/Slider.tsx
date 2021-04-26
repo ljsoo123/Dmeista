6c803acf6169488a4648a8be1af7f1f5b2beaab3
ljsoo123 <ljsoo123456@naver.com>
2021-04-26 19:04:22 +0900

    [MODIFY] 슬라이더 버튼 추가

--- src/components/Main/Post/Slider.tsx
+++ src/components/Main/Post/Slider.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import * as S from "../../../styles/Slider";
+import prev from "../../../../image/prev.svg";
+import next from "../../../../image/next.svg";
 
 const Slider = (props: { images: string[]; total: number }) => {
   const { images, total } = props;
@@ -8,7 +10,6 @@ const Slider = (props: { images: string[]; total: number }) => {
   const slideRef = useRef(null);
   const nextSlide = () => {
     if (currentSlide >= totalSlide) {
-      // 더 이상 넘어갈 슬라이드가 없으면 슬라이드를 초기화합니다.
       setCurrentSlide(0);
     } else {
       setCurrentSlide(currentSlide + 1);
@@ -23,6 +24,7 @@ const Slider = (props: { images: string[]; total: number }) => {
   };
   useEffect(() => {
     console.log(total);
+    console.log(images);
     setTotalSlide(2);
     slideRef.current.style.transition = "all 0.5s ease-in-out";
     slideRef.current.style.transform = `translateX(-${currentSlide}00%)`;
@@ -30,14 +32,19 @@ const Slider = (props: { images: string[]; total: number }) => {
   return (
     <>
       <S.ImageDiv>
-        {currentSlide}
         <S.SliderContainer ref={slideRef}>
           {images.map((now, i) => {
-            return <img src={now} key={i} />;
+            return (
+              <>
+                <img src={now} key={i} />
+              </>
+            );
           })}
         </S.SliderContainer>
-        <S.Button onClick={prevSlide}>Previous Slide</S.Button>
-        <S.Button onClick={nextSlide}>Next Slide</S.Button>
+        <S.Button>
+          <img src={prev} onClick={prevSlide}></img>
+          <img src={next} onClick={nextSlide}></img>
+        </S.Button>
       </S.ImageDiv>
     </>
   );
