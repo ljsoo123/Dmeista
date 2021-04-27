31b2f65b1587654076808c5411677da94c0d0c1c
ljsoo123 <ljsoo123456@naver.com>
2021-04-27 22:30:54 +0900

    [MODIFY] 무한스크롤

--- src/styles/MainStyle.ts
+++ src/styles/MainStyle.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const MainDiv = styled.div`
-  width: 100vw;
-  height: calc(100vh - 50px);
+  width: calc(100vw-50px);
+  //height: calc(100vh - 50px);
   padding-top: 50px;
   margin: 0 auto;
   overflow-x: hidden;
