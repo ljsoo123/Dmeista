a891c195f5d9aa33bbba149082d95a14c895caf7
ljsoo123 <ljsoo123456@naver.com>
2021-10-22 09:38:28 +0900

    [MODIFY] 이모지 수정

--- src/components/Main/Post/EmojiClick.tsx
+++ src/components/Main/Post/EmojiClick.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import * as S from "../../../styles/EmojiClick";
 
 const EmojiClick = () => {
-  return;
+  return (
+    <>
+      <S.Main>
+        <S.MainDiv></S.MainDiv>
+      </S.Main>
+    </>
+  );
 };
 
 export default EmojiClick;
